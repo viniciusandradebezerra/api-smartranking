88e39b138ed44465692cc9ea63bd655ffa69096e
Vinicius Andrade <vinicius.bezerra@saudeid.com.br>
2024-09-18 19:27:53 -0300

    adicionar método privado atualizar jogador e implementar na função de criarAtualizarJogador

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -11,11 +11,29 @@ export class JogadoresService {
     private readonly logger = new Logger(JogadoresService.name);
 
     async criarAtualizarJogador(CriarJogadorDto: CriarJogadorDto): Promise<void> {
-        await this.criar(CriarJogadorDto);
+
+        const { email } = CriarJogadorDto;
+
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
+
+        if (jogadorEncontrado) {
+            await this.atualizar(jogadorEncontrado, CriarJogadorDto);
+        } else {
+            await this.criar(CriarJogadorDto);
+        }
+
     }
 
     async consultarTodosJogadores(): Promise<IJogador[]> {
         return await this.jogadores;
+    }
+
+    private atualizar(jogadorEncontrado: IJogador, criarJogadorDto: CriarJogadorDto): void {
+
+        const { nome } = criarJogadorDto;
+
+        jogadorEncontrado.nome = nome;
+
     }
 
     private criar(criarJogadorDto: CriarJogadorDto): void {
