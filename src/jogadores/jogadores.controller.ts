bdf92f0192550d100e3eb25b837bc3d9a1266066
Vinicius Andrade <vinicius.bezerra@saudeid.com.br>
2024-09-18 19:50:04 -0300

    Implementação da rota detalarJogador usando o email como QueryParams

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { IJogador } from './interfaces/jogador.interface';
@@ -23,5 +23,12 @@ export class JogadoresController {
             return await this.jogadoresService.consultarJogadoresPeloEmail(email);
         }
         return await this.jogadoresService.consultarTodosJogadores();
+    }
+
+    @Delete()
+    async deletarJogador(
+        @Query('email') email: string
+    ): Promise<void> {
+        this.jogadoresService.deletarJogador(email);
     }
 }
