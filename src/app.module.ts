7d3b30c06739190ad13abb4339a2c91e7e15e6f1
Vinicius Andrade <vinicius.bezerra@saudeid.com.br>
2024-09-18 22:02:30 -0300

    Implementação do mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin123@cluster0.rlc0c.mongodb.net/smartranking?retryWrites=true&w=majority&appName=Cluster0'),
+    JogadoresModule],
   controllers: [],
   providers: [],
 })
